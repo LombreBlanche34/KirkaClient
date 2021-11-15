8107f1fc4d1d8e687b766892908a19c414c6a521
LombreBlanche34 <54815949+LombreBlanche34@users.noreply.github.com>
2021-11-15 16:22:39 +0100

    Add files via upload

--- src/preload/global.js
+++ src/preload/global.js
@@ -44,6 +44,7 @@ function doOnLoad() {
     resetVars();
     let html = `
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="${config.get("css")}">
     <style>
 
     #show-clientNotif{
